080c3417cc3d27dbd06c4221c35f382aa986defb
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-05-22 21:54:52 -0500

    Agregando landing y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './pages/welcome/landing/landing.component';
+import { LoginComponent } from './pages/welcome/login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome/welcome.component';
 
 const welcomeRoutes: Routes = [
-  {}
+  {path: 'landing', component: LandingComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'landing'}
 ];
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent, children: welcomeRoutes},
+  {path: '', pathMatch: 'full', redirectTo: 'welcome'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { FormLoginComponent } from './components/forms/form-login/form-login.component';
 import { NavWelcomeComponent } from './components/navbars/nav-welcome/nav-welcome.component';
 import { LandingComponent } from './pages/welcome/landing/landing.component';
+import { WelcomeComponent } from './pages/welcome/welcome/welcome.component';
+import { LoginComponent } from './pages/welcome/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormLoginComponent,
     NavWelcomeComponent,
-    LandingComponent
+    LandingComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
