3d4aae557962176de157e5923ddce764309e8ad1
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-05-25 18:50:32 -0500

    Cambios en el css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,15 +12,11 @@ import { ContactComponent } from './pages/welcome/contact/contact.component';
 import { AdoptADogComponent } from './pages/welcome/adopt-adog/adopt-adog.component';
 import { AdoptACatComponent } from './pages/welcome/adopt-acat/adopt-acat.component';
 
-const adoptRoutes: Routes = [
-  {path: 'adopt-dogs', component: AdoptADogComponent},
-  {path: 'adopt-cats', component: AdoptACatComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'adopt-dogs'}
-];
-
 const welcomeRoutes: Routes = [
   {path: 'landing', component: LandingComponent},
-  {path: 'adopt', component: AdoptComponent, children: adoptRoutes},
+  {path: 'adopt', component: AdoptComponent},
+  {path: 'adopt-dogs', component: AdoptADogComponent},
+  {path: 'adopt-cats', component: AdoptACatComponent},
   {path: 'lost', component: LostComponent},
   {path: 'news', component: NewsComponent},
   {path: 'dates', component: DatesComponent},
