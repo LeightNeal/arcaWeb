fe341ed13c9e081277fd547ece2367f0bc14110a
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-05-27 22:59:19 -0500

    Agregando menú hamburguesa y banner de la sección de citas

--- src/app/components/navbars/nav-welcome/nav-welcome.component.ts
+++ src/app/components/navbars/nav-welcome/nav-welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-nav-welcome',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavWelcomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('sideMenu') menuSide: ElementRef;
+
+  constructor() {}
 
   ngOnInit() {
   }
 
+  changeClass() {
+    this.menuSide.nativeElement.classList.toggle('nav-active');
+    console.log('botón cliqueado');
+  }
 }
