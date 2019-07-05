69857d00a7a516bc9ca1d8db04bcdde0f94de0b6
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-07-05 00:41:24 -0500

    Recuperé el funcionamiento de iniciar sesión

--- src/app/components/forms/form-login/form-login.component.ts
+++ src/app/components/forms/form-login/form-login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-form-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormLoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('txtEmail') email: ElementRef;
+  @ViewChild('txtPassword') password: ElementRef;
+
+  constructor(private authService: AuthService, private usuarioService: UsuarioService,
+    private router: Router) { }
 
   ngOnInit() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+
+  async iniciarSesion() {
+    if (this.email.nativeElement.value !== '' && this.password.nativeElement.value !== '') {
+      try {
+        const usuario = await this.authService.iniciarSesion(`${this.email.nativeElement.value}`, `${this.password.nativeElement.value}`);
+        if (usuario) {
+          const usuarioRegistro = await this.usuarioService.obtenerUsuario(usuario.user.uid);
+          usuarioRegistro.subscribe(
+            data => {
+              alert('Iniciaste sesión');
+              localStorage.setItem('usuarioActual', JSON.stringify(data));
+              location.reload();
+              this.router.navigate(['/welcome', 'landing']);
+            }, error => console.log(error));
+        }
+      } catch (error) {
+        alert('Error al iniciar sesión.');
+      }
+    } else {
+      alert('Llena todos los campos');
+    }
   }
 
 }
